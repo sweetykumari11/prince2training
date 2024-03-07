2bcaec527e0d4c9ea9cef6b7bfcb7ecd60c266f9
sweety kumari <sweety.kumari@theknowledgeacademy.com>
2024-03-07 12:18:01 +0530

    course

--- resources/views/course/edit.blade.php
+++ resources/views/course/edit.blade.php
@@ -126,11 +126,7 @@
                                         <div class="timeline-item">
                                             <div class="card-header">
                                                 <h3 class="card-title">
-                                                    @if($activity->created_by)
-                                                        {{ $activity->creator->name }}
-                                                    @else
-                                                        System
-                                                    @endif
+                                                    <h3 class="card-title">{{ $activity->creator->name ?? '' }}</h3>
                                                 </h3>
                                             </div>
                                             <h3 class="timeline-header no-border"> {{ $activity->activity }} </a></h3>
