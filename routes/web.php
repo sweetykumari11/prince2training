ddeeb566f5ac02ddd6566b18d48b1f4046f951c6
sherinbn98 <162095420+sherinbn98@users.noreply.github.com>
2024-03-08 10:20:14 +0530

    route

--- routes/web.php
+++ routes/web.php
@@ -34,48 +34,50 @@ use App\Http\Controllers\CoursedetailController;
 
 
 Route::get('/',             [AuthController::class, 'index'])->name('login');
-Route::get('/login',        [AuthController::class, 'index'])->name('login');
-Route::post('custom-login', [AuthController::class, 'customLogin'])->name('login.custom');
+Route::prefix('admin')->group(function () {
+    Route::get('/login',        [AuthController::class, 'index'])->name('login');
+    Route::post('custom-login', [AuthController::class, 'customLogin'])->name('login.custom');
 
-// Password Reset Routes
-Route::get('/password/reset/{id}', [UserController::class, 'Reset'])->name('password.reset');
-Route::get('/reset-password/{id}', [UserController::class, 'showResetForm'])->name('reset.password');
-Route::post('/change-password', [UserController::class, 'changepassword'])->name('password.change');
+    // Password Reset Routes
+    Route::get('/password/reset/{id}', [UserController::class, 'Reset'])->name('password.reset');
+    Route::get('/reset-password/{id}', [UserController::class, 'showResetForm'])->name('reset.password');
+    Route::post('/change-password', [UserController::class, 'changepassword'])->name('password.change');
 
-Route::group(['middleware' => ['auth:web']], function () {
+    Route::group(['middleware' => ['auth:web']], function () {
 
-    Route::get('dashboard',     [HomeController::class, 'index'])->name('dashboard.index');
-    Route::resource('user',            UserController::class);
-    Route::resource('role',            RoleController::class);
-    Route::resource('module',          ModuleController::class);
-    Route::resource('permission',      PermissionController::class);
-    Route::resource('pagecontent',      PageContentController::class);
-    Route::resource('blogs',            BlogController::class);
-    Route::resource('tag',              TagController::class);
-    Route::resource('course',           CourseController::class);
-    Route::resource('countries',              CountryController::class);
-    Route::resource('category',         CategoryController::class);
-    Route::resource('region', RegionController::class);
-    Route::resource('locations', LocationsController::class);
-    Route::resource('blogs.blogDetail',         BlogDetailController::class);
-    Route::resource('topic',            TopicController::class);
-    Route::resource('topic.faqs',               FaqController::class);
-    Route::resource('topic.topicdetails',       TopicDetailController::class);
-    Route::resource('course.coursedetails',     CoursedetailController::class);
-    Route::resource('course.faqs',              FaqController::class);
+        Route::get('dashboard',     [HomeController::class, 'index'])->name('dashboard.index');
+        Route::resource('user',            UserController::class);
+        Route::resource('role',            RoleController::class);
+        Route::resource('module',          ModuleController::class);
+        Route::resource('permission',      PermissionController::class);
+        Route::resource('pagecontent',      PageContentController::class);
+        Route::resource('blogs',            BlogController::class);
+        Route::resource('tag',              TagController::class);
+        Route::resource('course',           CourseController::class);
+        Route::resource('countries',              CountryController::class);
+        Route::resource('category',         CategoryController::class);
+        Route::resource('region', RegionController::class);
+        Route::resource('locations', LocationsController::class);
+        Route::resource('blogs.blogDetail',         BlogDetailController::class);
+        Route::resource('topic',            TopicController::class);
+        Route::resource('topic.faqs',               FaqController::class);
+        Route::resource('topic.topicdetails',       TopicDetailController::class);
+        Route::resource('course.coursedetails',     CoursedetailController::class);
+        Route::resource('course.faqs',              FaqController::class);
 
 
-    Route::get('changeblogStatus',        [BlogController::class, 'blogStatus']);
-    Route::get('changecountryStatus',        [CountryController::class, 'countryStatus']);
-    Route::get('blogsetpopular', [BlogController::class, 'setPopular']);
-    Route::post('logout',       [AuthController::class, 'logout'])->name('logout');
-    Route::get('changecategoryStatus',    [CategoryController::class, 'categoryStatus']);
-    Route::post('rolesupdate',       [UserController::class, 'rolesupdate'])->name('user.rolesupdate');
-    Route::get('actvities', [LogActivityController::class, 'index'])->name('actvities.index');
+        Route::get('changeblogStatus',        [BlogController::class, 'blogStatus']);
+        Route::get('changecountryStatus',        [CountryController::class, 'countryStatus']);
+        Route::get('blogsetpopular', [BlogController::class, 'setPopular']);
+        Route::post('logout',       [AuthController::class, 'logout'])->name('logout');
+        Route::get('changecategoryStatus',    [CategoryController::class, 'categoryStatus']);
+        Route::post('rolesupdate',       [UserController::class, 'rolesupdate'])->name('user.rolesupdate');
+        Route::get('actvities', [LogActivityController::class, 'index'])->name('actvities.index');
 
-    Route::get('changefaqStatus',         [FaqController::class, 'faqStatus']);
-    Route::get('changetopicStatus',       [TopicController::class, 'updateStatus']);
-    Route::get('country-topics',        [TopicController::class, 'storeTopicCountry']);
-    Route::get('topicsetpopular',       [TopicController::class, 'setPopular']);
+        Route::get('changefaqStatus',         [FaqController::class, 'faqStatus']);
+        Route::get('changetopicStatus',       [TopicController::class, 'updateStatus']);
+        Route::get('country-topics',        [TopicController::class, 'storeTopicCountry']);
+        Route::get('topicsetpopular',       [TopicController::class, 'setPopular']);
 
+    });
 });
